c57c0edf0ed19957023a62d671b885fa250e46ab
dcollier <dj@djcollier.com>
2020-01-02 17:24:25 -0500

    Work on 2019 day 05

--- 2019/day05/day05.js
+++ 2019/day05/day05.js
@@ -3,13 +3,27 @@ import { readFileSync } from 'fs';
 const input = readFileSync('./day05/day05-input.txt', 'utf-8');
 const initMemory = input.split(',').map(n => parseInt(n));
 
-// function intcode(initMemory) {
-//   let memo = initMemory.slice();
-//   let i = 0;
-//   while(i < memo.length) {
-//     const instructions = memo[i].toString();
-//     const opcode = instructions.substring(instructions.length - 2);
-//   }
-// }
+function intcode(initMemory) {
+  let memo = initMemory.slice();
+  let i = 0;
+  while(i < memo.length) {
+    const instructions = memo[i].toString();
+    let opcode = instructions.substring(instructions.length - 2);
+    const modes = instructions.substring(0, instructions.length - 2);
+    console.log(instructions);
+    opcode = opcode.length === 1 ? '0' + opcode : opcode;
+    if(opcode === '01') { // addition
+      i += 4;
+    } else if(opcode === '02') { // multiply
+      i += 4;
+    } else if(opcode === '03') { // input update
+      i += 2;
+    } else if(opcode === '04') { // output
+      i += 2;
+    } else if(opcode === '99') {
+      i+= 1;
+    }
+  }
+}
 
-console.log(initMemory);
+intcode(initMemory);
