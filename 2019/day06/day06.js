0e07fad5acc07554dcc45b41138168fb5c3fbdda
dcollier <dj@djcollier.com>
2020-01-07 12:48:01 -0500

    Work on 2019 day06

--- 2019/day06/day06.js
+++ 2019/day06/day06.js
@@ -1,5 +1,44 @@
 import { readFileSync } from 'fs';
 
 const input = readFileSync('./day06/day06-input.txt', 'utf-8');
+// const orbits = input.split('\r\n').map(rel => rel.split(')'));
 
-console.log(input);
+const orbits = [
+  ['COM','B'],
+  ['B','C'],
+  ['C','D'],
+  ['D','E'],
+  ['E','F'],
+  ['B','G'],
+  ['G','H'],
+  ['D','I'],
+  ['E','J'],
+  ['J','K'],
+  ['K','L']
+];
+
+// Make COM the first orbit map entry
+const comIndex = orbits.findIndex(orbit => {
+  return orbit[0] === 'COM';
+});
+[orbits[0], orbits[comIndex]] = [orbits[comIndex], orbits[0]];
+
+function buildTree(orbit) {
+  const [m0, m1] = orbit;
+  const childOrbits = orbits.filter(orbit => orbit[0] === m0);
+  if(!childOrbits.length) {
+    return {
+      value: m0,
+      nodes: [{value: m1}]
+    };
+  }
+  const childNodes = childOrbits.map(childOrbit => buildTree(childOrbit));
+  return {
+    value: m0,
+    nodes: childNodes
+  };
+}
+
+const orbitTree = buildTree(orbits[0]);
+
+console.log(orbitTree);
