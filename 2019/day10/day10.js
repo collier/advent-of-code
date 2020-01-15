9a1e84e171645dfbd8486f059e87d665e42cc52d
dcollier <dj@djcollier.com>
2020-01-15 17:18:44 -0500

    Work on 2019 day 10

--- 2019/day10/day10.js
+++ 2019/day10/day10.js
@@ -3,36 +3,41 @@ import { readFileSync } from 'fs';
 const input = readFileSync('./day10/day10-input.txt', 'utf-8');
 const space = input.split('\r\n').map(row => row.split(''));
 
-function findSlope(p0, p1) {
-  const [ p1x, p1y ] = p1;
-  const [ p0x, p0y ] = p0;
-  return (p1y - p1x) / (p0y - p0x);
+function findSlope(p1, p2) {
+  const [ x1, y1 ] = p1;
+  const [ x2, y2 ] = p2;
+  return (y2 - y1) / (x2 - x1);
 }
 
-function findVisibleAsteroids(px, py) {
-  let results = [];
-  let slopes = {};
+function countVisibleAsteroids(px, py) {
+  const slopes = new Set();
   for(let x = 0; x < space.length; x++) {
     for(let y = 0; y < space[x].length; y++) {
-      if(x !== px && y !== py) {
+      if(!(x === px && y === py) && space[x][y] === '#') {
         const slope = findSlope([px, py],[x, y]);
-        if(!slopes[slope]) {
-          results.push([x, y]);
-          slopes[slope] = true;
-        }
+        console.log([px, py],[x, y], slope);
+        slopes.add(slope);
       }
     }
   }
-  return results.length;
+  return slopes.size;
 } 
 
-let max = 0;
-for(let x = 0; x < space.length; x++) {
-  for(let y = 0; y < space[x].length; y++) {
-    var visibleAsteroids = findVisibleAsteroids(x, y);
-    if(visibleAsteroids > max) {
-      max = visibleAsteroids;
-    }
-    console.log(x, y, visibleAsteroids);
-  }
-}
+countVisibleAsteroids(3,4);
+
+// let max = 0;
+// let maxPoint;
+// for(let x = 0; x < space.length; x++) {
+//   for(let y = 0; y < space[x].length; y++) {
+//     if(space[x][y] === '#') {
+//       const visibleAsteroids = countVisibleAsteroids(x, y);
+//       console.log([x, y], visibleAsteroids);
+//       if(visibleAsteroids > max) {
+//         max = visibleAsteroids;
+//         maxPoint = [x, y];
+//       }
+//     }
+//   }
+// }
+
+// console.log(maxPoint, max);
