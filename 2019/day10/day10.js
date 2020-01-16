b31e82bb522b939d6b261bf953e9e379f0862db6
dcollier <dj@djcollier.com>
2020-01-16 15:26:23 -0500

    Finishes 2019 day 10

--- 2019/day10/day10.js
+++ 2019/day10/day10.js
@@ -6,38 +6,110 @@ const space = input.split('\r\n').map(row => row.split(''));
 function findSlope(p1, p2) {
   const [ x1, y1 ] = p1;
   const [ x2, y2 ] = p2;
-  return (y2 - y1) / (x2 - x1);
+  const slope = (y2 - y1) / (x2 - x1);
+  let quad;
+  if(x2 >= x1 && y2 < y1) {
+    quad = 1;
+  } else if(x2 > x1 && y2 >= y1) {
+    quad = 2;
+  } else if(x2 <= x1 && y2 > y1) {
+    quad = 3;
+  } else if(x2 < x1 && y2 <= y1) {
+    quad = 4;
+  }
+  return [ slope, quad ];
 }
 
-function countVisibleAsteroids(px, py) {
-  const slopes = new Set();
-  for(let x = 0; x < space.length; x++) {
-    for(let y = 0; y < space[x].length; y++) {
-      if(!(x === px && y === py) && space[x][y] === '#') {
-        const slope = findSlope([px, py],[x, y]);
-        console.log([px, py],[x, y], slope);
-        slopes.add(slope);
+function getSlopeMap(px, py) {
+  const slopeMap = new Map();
+  for(let y = 0; y < space.length; y++) {
+    for(let x = 0; x < space[y].length; x++) {
+      if(!(x === px && y === py) && space[y][x] === '#') {
+        const [ slope, quad ] = findSlope([px, py], [x, y]);
+        const key = slope + ',' + quad;
+        if(!slopeMap.has(key)) {
+          slopeMap.set(key, [[x, y]]);
+        } else {
+          const newPoints = [...slopeMap.get(key), [x, y]];
+          if(quad === 1 || quad === 4) {
+            newPoints.sort((a, b) => (a[0] + a[1]) - (b[0] + b[1]));
+          } else {
+            newPoints.sort((a, b) => (b[0] + b[1]) - (a[0] + a[1]));
+          }
+          slopeMap.set(key, newPoints);
+        }
       }
     }
   }
-  return slopes.size;
-} 
+  return slopeMap;
+}
 
-countVisibleAsteroids(3,4);
+function getOrderedSlopesByQuad(slopeMap, targetQuad) {
+  const result = [];
+  for(let key of slopeMap.keys()) {
+    const [ slope, quad ] = key.split(',').map(n => parseFloat(n));
+    if(quad === targetQuad) {
+      result.push(slope);
+    }
+  }
+  result.sort((a, b) => a - b);
+  if(result[result.length - 1] === Infinity) {
+    result.unshift(result.pop());
+  }
+  return result;
+}
 
-// let max = 0;
-// let maxPoint;
-// for(let x = 0; x < space.length; x++) {
-//   for(let y = 0; y < space[x].length; y++) {
-//     if(space[x][y] === '#') {
-//       const visibleAsteroids = countVisibleAsteroids(x, y);
-//       console.log([x, y], visibleAsteroids);
-//       if(visibleAsteroids > max) {
-//         max = visibleAsteroids;
-//         maxPoint = [x, y];
-//       }
-//     }
-//   }
-// }
+function findNthVaporizedPoint(point, n) {
+  let vaporizedCount = 0;
+  let lastVaporized;
+  const slopeMap = getSlopeMap(...point);
+  while(slopeMap.size > 0) {
+    for(let i = 1; i <= 4; i++) {
+      const slopes = getOrderedSlopesByQuad(slopeMap, i);
+      for(const slope of slopes) {
+        const key = slope + ',' + i;
+        const points = slopeMap.get(key);
+        lastVaporized = points.pop();
+        vaporizedCount++;
+        if(vaporizedCount === n) {
+          break;
+        }
+        if(!points.length) {
+          slopeMap.delete(key);
+        }
+      }
+      if(vaporizedCount === n) {
+        break;
+      }
+    }
+    if(vaporizedCount === n) {
+      break;
+    }
+  }
+  return lastVaporized;
+}
 
-// console.log(maxPoint, max);
+function findHighestVisibilityAsteroid() {
+  let max = 0;
+  let maxPoint;
+  for(let y = 0; y < space.length; y++) {
+    for(let x = 0; x < space[y].length; x++) {
+      if(space[y][x] === '#') {
+        const visibleAsteroids = getSlopeMap(x, y).size;
+        if(visibleAsteroids > max) {
+          max = visibleAsteroids;
+          maxPoint = [x, y];
+        }
+      }
+    }
+  }
+  return [ max, maxPoint ];
+}
+
+const [ maxVisibleAsteroids, maxPoint ] = findHighestVisibilityAsteroid();
+const [ vapX, vapY ] = findNthVaporizedPoint(maxPoint, 200);
+const answer1 = maxVisibleAsteroids;
+const answer2 = (vapX * 100) + vapY;
+
+console.log(`Answer 1: ${answer1}`); // 247
+console.log(`Answer 2: ${answer2}`); // 1919
